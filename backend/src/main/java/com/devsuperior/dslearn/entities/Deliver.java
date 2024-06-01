ecf99ec9cccb51bf13665e96b0be004ead2cda96
Felipe Neves <felipelabs@gmail.com>
2024-05-31 22:18:18 -0300

    Add and fix entities

--- backend/src/main/java/com/devsuperior/dslearn/entities/Deliver.java
+++ backend/src/main/java/com/devsuperior/dslearn/entities/Deliver.java
@@ -4,6 +4,7 @@ import com.devsuperior.dslearn.entities.enums.DeliverStatus;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.Objects;
 
 @Entity
 @Table(name = "tb_deliver")
@@ -114,10 +115,7 @@ public class Deliver {
 
     @Override
     public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
+        return Objects.hash(id);
     }
 
     @Override
@@ -129,11 +127,6 @@ public class Deliver {
         if (getClass() != obj.getClass())
             return false;
         Deliver other = (Deliver) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
+        return Objects.equals(id, other.id);
     }
 }
